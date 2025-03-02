f848ff01c47e227ff2233f1859441fb7181e507d
oscarhernandeziv <oscarhernandeziv@gmail.com>
2025-03-01 22:33:25 -0800

    title/tagline

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,10 @@ export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <h1 className="text-4xl font-bold">Hello World</h1>
+        <h1 className="text-4xl font-bold">Uncoder</h1>
+        <p className="text-lg">
+          A transparent barcode generator for people with work to do.
+        </p>
       </main>
     </div>
   );
